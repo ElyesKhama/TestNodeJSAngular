95dc65d6a7195406834f09a8565619cca69cd681
KHAMASSI <elyes.khamassi@cgi.com>
2019-03-01 11:13:49 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-angular-project';
+
+export class AppComponent implements OnInit {
+  title = 'my first Angular Project ';
+  constructor() {
+  }
+
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+  }
 }
